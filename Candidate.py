d1ce8f3e0b22748d3a6862f84c3123d4f5b89833
Owwwen86 <smirnovkirill@mail.ru>
2022-08-12 13:20:43 +0300

    Homework_11 done

---
+++ Candidate.py
@@ -0,0 +1,9 @@
+class Candidate:
+    def __init__(self, id, name, picture, position, gender, age, skills):
+        self.id = id
+        self.name = name
+        self.picture = picture
+        self.gender = gender
+        self.age = age
+        self.position = position
+        self.skills = skills
